ce1605ff0e9eb87e93f4af08d383a4fd45b2422c
Howard Grimberg <hgrimberg01@gmail.com>
2015-03-10 21:50:04 -0700

    Mar 10 	Labor Day in South Korea Mar 10 	Andreas Klemm <andreas@FreeBSD.org> born in Duesseldorf, Nordrhein-Westfalen, Germany, 1963 Mar 10 	N'oubliez pas les Vivien !

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 random="$(openssl rand 2 | od -DAn | xargs)";
-message="$(calendar | head -n 2)";
+message="$(calendar | head -n 3)";
 cd ~/aughing-octo-dangerzone;
 git pull > /dev/null;
 head -c "$random" /dev/urandom > somefile.txt;
-echo $message >> somefile.txt;
+calendar | head -n "$random" >> somefile.txt;
 git add . ;
 git commit -m "$message" > /dev/null;
 git push origin master;
