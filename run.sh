7c417e668b7b3bad01bd743717011d53ad7d113f
Howard Grimberg <hgrimberg01@gmail.com>
2015-03-10 21:28:11 -0700

    Mar 10 	Labor Day in South Korea Mar 10 	Andreas Klemm <andreas@FreeBSD.org> born in Duesseldorf, Nordrhein-Westfalen, Germany, 1963

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+random="$(openssl rand 1 | od -DAn | xargs)";
 message="$(calendar | head -n 2)";
 cd ~/aughing-octo-dangerzone;
 git pull > /dev/null;
-head -c 32 /dev/random > somefile.txt;
+head -c $random /dev/random > somefile.txt;
 echo $message >> somefile.txt;
 git add . ;
 git commit -m "$message" > /dev/null;
