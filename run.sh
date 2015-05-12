0ecffd0d7d6fbd44ae8fe140485d914a0a60bba4
Howard Grimberg <hgrimberg01@gmail.com>
2015-05-12 02:04:02 +0000

    May 2015 Su Mo Tu We Th Fr Sa                 1  2

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 random="$(openssl rand 1 | od -DAn | xargs)";
-message="$(calendar | head -n 3)";
+message="$(cal | head -n 3)";
 cd ~/aughing-octo-dangerzone;
 git pull > /dev/null;
 head -c "$random" /dev/urandom > somefile.txt;
-calendar | head -n "$random" >> somefile.txt;
+cal | head -n "$random" >> somefile.txt;
 git add . ;
 git commit -m "$message" > /dev/null;
 git push origin master;
